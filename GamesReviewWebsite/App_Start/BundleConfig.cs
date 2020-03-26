a07b911aadaa579462838780e2b098f3e58c7948
Omari Thompson <omarion_203@msn.com>
2020-03-26 07:12:23 +0000

    Added the final touches to the website to make sure you can navigate it and some small quality of life updates.

--- GamesReviewWebsite/App_Start/BundleConfig.cs
+++ GamesReviewWebsite/App_Start/BundleConfig.cs
@@ -20,10 +20,11 @@ namespace GamesReviewWebsite
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
--- GamesReviewWebsite/Models/GamesCollectionContext.cs
+++ GamesReviewWebsite/Models/GamesCollectionContext.cs
@@ -21,4 +21,19 @@ namespace GamesReviewWebsite.Models
 
         public System.Data.Entity.DbSet<GamesReviewWebsite.Models.GamesCollection> GamesCollections { get; set; }
     }
+    public class GamesListContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public GamesListContext() : base("name=GamesListContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<GamesReviewWebsite.Models.GamesCollection> GamesCollections { get; set; }
+    }
 }
